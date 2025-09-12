0177dc2b8b22022572e4db5884d219794492b1db
Zied Bouazizi <zied.bouazizi@outlook.com>
2025-09-12 09:56:17 +0100

    Implement displaying tasks

--- resources/js/constants.jsx
+++ resources/js/constants.jsx
@@ -8,3 +8,13 @@ export const PROJECT_STATUS_TEXT_MAP = {
     in_progress: "In Progress",
     completed: "Completed",
 };
+export const TASK_STATUS_CLASS_MAP = {
+    pending: "bg-amber-500",
+    in_progress: "bg-blue-500",
+    completed: "bg-green-500",
+};
+export const TASK_STATUS_TEXT_MAP = {
+    pending: "Pending",
+    in_progress: "In Progress",
+    completed: "Completed",
+};
