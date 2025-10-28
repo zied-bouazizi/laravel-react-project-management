77b1da71a793e7cce47e8f199995b8fcb524c349
Zied Bouazizi <zied.bouazizi@outlook.com>
2025-10-28 16:58:33 +0100

    Make my active tasks table responsive on mobile

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -62,47 +62,49 @@ export default function Dashboard({
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             <h3 className="text-gray-200 text-xl font-semibold">
                                 My Active Tasks
-                            </h3>
+                                </h3>
 
-                            <table className="mt-3 w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
-                                    <tr>
-                                        <th className="px-3 py-3">ID</th>
-                                        <th className="px-3 py-3">Project Name</th>
-                                        <th className="px-3 py-3">Name</th>
-                                        <th className="px-3 py-3">Status</th>
-                                        <th className="px-3 py-3">Due Date</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {activeTasks.data.map((task) => (
-                                        <tr key={task.id}>
-                                            <td className="px-3 py-2">{task.id}</td>
-                                            <td className="px-3 py-2 text-white hover:underline">
-                                                <Link href={route("project.show", task.project.id)}>
-                                                    {task.project.name}
-                                                </Link>
-                                            </td>
-                                            <td className="px-3 py-2 text-white hover:underline">
-                                                <Link href={route("task.show", task.id)}>
-                                                    {task.name}
-                                                </Link>
-                                            </td>
-                                            <td className="px-3 py-2">
-                                                <span
-                                                    className={
-                                                        "px-2 py-1 rounded text-nowrap text-white " +
-                                                        TASK_STATUS_CLASS_MAP[task.status]
-                                                    }
-                                                >
-                                                    {TASK_STATUS_TEXT_MAP[task.status]}
-                                                </span>
-                                            </td>
-                                            <td className="px-3 py-2 text-nowrap">{task.due_date}</td>
+                            <div className="overflow-x-auto mt-3">
+                                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                        <tr>
+                                            <th className="px-3 py-3">ID</th>
+                                            <th className="px-3 py-3">Project Name</th>
+                                            <th className="px-3 py-3">Name</th>
+                                            <th className="px-3 py-3">Status</th>
+                                            <th className="px-3 py-3">Due Date</th>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        {activeTasks.data.map((task) => (
+                                            <tr key={task.id}>
+                                                <td className="px-3 py-2">{task.id}</td>
+                                                <td className="px-3 py-2 text-white hover:underline">
+                                                    <Link href={route("project.show", task.project.id)}>
+                                                        {task.project.name}
+                                                    </Link>
+                                                </td>
+                                                <td className="px-3 py-2 text-white hover:underline">
+                                                    <Link href={route("task.show", task.id)}>
+                                                        {task.name}
+                                                    </Link>
+                                                </td>
+                                                <td className="px-3 py-2">
+                                                    <span
+                                                        className={
+                                                            "px-2 py-1 rounded text-nowrap text-white " +
+                                                            TASK_STATUS_CLASS_MAP[task.status]
+                                                        }
+                                                    >
+                                                        {TASK_STATUS_TEXT_MAP[task.status]}
+                                                    </span>
+                                                </td>
+                                                <td className="px-3 py-2 text-nowrap">{task.due_date}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
