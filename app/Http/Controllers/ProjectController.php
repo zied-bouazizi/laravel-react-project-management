94557cf2c2a05e685bbddc519fb6dacd4b488b80
Zied Bouazizi <zied.bouazizi@outlook.com>
2025-10-28 18:20:36 +0100

    Implement updating project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -103,7 +103,9 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        //
+        return inertia("Project/Edit", [
+            'project' => new ProjectResource($project),
+        ]);
     }
 
     /**
@@ -111,7 +113,21 @@ class ProjectController extends Controller
      */
     public function update(UpdateProjectRequest $request, Project $project)
     {
-        //
+        $data = $request->validated();
+            /** @var $image \Illuminate\Http\UploadedFile */
+            $image = $data['image'] ?? null;
+            $data['updated_by'] = Auth::id();
+            if ($image) {
+                if ($project->image_path) {
+                    Storage::disk('public')->deleteDirectory(dirname($project->image_path));
+                }
+
+                $data['image_path'] = $image->store('project/' . Str::random(), 'public');
+            }
+            $project->update($data);
+
+            return to_route('project.index')
+                ->with('success', "Project \"$project->name\" was updated"); 
     }
 
     /**
