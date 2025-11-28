0f50512f61f1d9b3188671f338efa62964d36698
Zied Bouazizi <zied.bouazizi@outlook.com>
2025-11-28 14:16:16 +0100

    Hide missing images for project and task

--- resources/js/Pages/Task/Show.jsx
+++ resources/js/Pages/Task/Show.jsx
@@ -24,13 +24,16 @@ export default function Show({ task }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div>
-                            <img
-                                src={task.image_path}
-                                alt=""
-                                className="w-full h-64 object-cover"
-                            />
-                        </div>
+                        {task.image_path && (
+                            <div>
+                                <img
+                                    src={task.image_path}
+                                    alt=""
+                                    className="w-full h-64 object-cover"
+                                />
+                            </div>
+                        )}
+
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             <div className="grid gap-1 grid-cols-2 mt-2">
                                 <div>
